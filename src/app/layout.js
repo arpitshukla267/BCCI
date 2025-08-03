733f47f1a438ea8c0b0411c8e99529fe5888072e
Arpit Shukla <shuklaarpit440@email.com>
2025-08-03 15:26:22 +0530

    debugging code

--- src/app/layout.js
+++ src/app/layout.js
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} antialiased`}
       >
-        {/* <TawkToScript /> */}
+        <TawkToScript />
         {children}
       </body>
     </html>
